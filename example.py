904aa75928235d0bc722b62ffcf3e22f050fe7a5
sandstorm12 <sandstormeatwo@gmail.com>
2022-01-08 12:37:31 +0000

    Example added

---
+++ example.py
@@ -0,0 +1,13 @@
+"""An example usage of the implemented module
+
+Example should be a bare-minimum application of the module with all unncessary
+details removed.
+The aim is to show a new user how to start using the module in the fastest way.
+"""
+
+from aiml_template import SampleClass
+
+
+if __name__ == "__main__":
+    sample_class = SampleClass()
+    sample_class.sample_method()
