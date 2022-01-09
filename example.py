555488601427f53cf9316d6eef8dc1ef40397932
sandstorm12 <sandstormeatwo@gmail.com>
2022-01-09 15:41:51 +0000

    Example updated
    - Example.py contains multiple files
    - Example image added

--- example.py
+++ example.py
@@ -5,9 +5,44 @@ details removed.
 The aim is to show a new user how to start using the module in the fastest way.
 """
 
+import cv2
+
 from aiml_template import SampleClass
+from aiml_template import SampleClassVisualize
+
+
+def example_sample_class():
+    image = cv2.imread("./example.png")
+
+    sample_class = SampleClass()
+    response = sample_class.sample_method(image)
+
+    print("Response: {}".format(response))
+
+
+def example_sample_class_visualize():
+    image = cv2.imread("./example.png")
+
+    sample_class = SampleClassVisualize()
+    response, image_visualized = sample_class.sample_method_visualize(image)
+
+    print("Image: {} Response: {}".format(
+            image_visualized.shape, response
+        )
+    )
+
+
+def example_print():
+    print("This is an example")
 
 
 if __name__ == "__main__":
-    sample_class = SampleClass()
-    sample_class.sample_method()
+    print("Running examples\n{}".format('-' * 20))
+
+    for key in list(locals()):
+        if callable(locals()[key]) and "example" in key:
+            print("Running '{}'...\n".format(key))
+            locals()[key]()
+            print("{}".format('-' * 20))
+    
+    print("Done running examples.")
