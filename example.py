5d0602cb66998688a32a9ac4b6b230429332805c
xiima-fanap <121428718+xiima-fanap@users.noreply.github.com>
2022-12-27 14:45:59 +0330

    Update example.py

--- example.py
+++ example.py
@@ -11,30 +11,30 @@ from aiml_template import SampleClass
 from aiml_template import SampleClassVisualize
 
 
-def example_sample_class():
-    image = cv2.imread("./sample.png")
+def example_sample_class(image :np.ndarray)-> str:
+    """Sample method to do a great 
+        
+        Parameters
+        ----------
+        image : np.ndarray
+            The image to process
+
+        Returns
+        -------
+        str
+            Response in json format
+
+        Raises
+        ------
+        ExceptionInvalidInput
+            If input type is not np.ndarray
+     """
+    image = cv2.imread(image)
 
     sample_class = SampleClass()
     response = sample_class.sample_method(image)
 
-    print("Response: {}".format(response))
-
-
-def example_sample_class_visualize():
-    image = cv2.imread("./sample.png")
-
-    sample_class = SampleClassVisualize()
-    response, image_visualized = sample_class.sample_method_visualize(image)
-
-    print("Image: {} Response: {}".format(
-            image_visualized.shape, response
-        )
-    )
-
-
-def example_print():
-    print("This is an example")
-
+     return "{Response: {}}".format(response)
 
 if __name__ == "__main__":
     print("Running examples\n{}".format('-' * 20))
